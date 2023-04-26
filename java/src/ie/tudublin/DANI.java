a7c4c593380cb3372fed5443ef11c49e09d2a7d6
Joanne <Joanneumeh123@gmail.com>
2023-04-26 17:03:00 +0100

    1

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 import processing.core.PApplet;
 
+import java.util.*;
+
+
 public class DANI extends PApplet {
 
 	
@@ -41,4 +44,63 @@ public class DANI extends PApplet {
         textAlign(CENTER, CENTER);
         
 	}
+	
+	   public void loadFile(String filename) {
+		  // load the file into a String array
+		  String[] lines = loadStrings(filename);
+		  // loop through each line in the file
+		  for (String line : lines) {
+			// split the line into an array of words
+			String[] words = split(line, ' ');
+			// loop through each word in the line
+			for (String word : words) {
+			  // remove punctuation characters from the word
+			  String cleanedWord = word.replaceAll("[^\\w\\s]", "");
+			  // convert the word to lower case
+			  String lowercaseWord = cleanedWord.toLowerCase();
+			  // add the word to the model
+			  addWordToModel(lowercaseWord);
+			}
+		  }
+		}
+	  
+		private void addWordToModel(String word) {
+		  // add the word to the model
+		  // implementation omitted for brevity
+		}
+		public Word findWord(String str, Word[] words) {
+			
+			for (Word w : words) {
+				if (w.getWord().equals(str)) {
+					return w;
+				}
+			}
+			return null;
+		}
+		
+		
+    private Map<String, Map<String, Integer>> model;
+
+    public void ModelPrinter(Map<String, Map<String, Integer>> model) {
+        this.model = model;
+    }
+
+    public void printModel() {
+        for (String word : model.keySet()) {
+            System.out.print(word + ": ");
+            Map<String, Integer> nextWords = model.get(word);
+            for (String nextWord : nextWords.keySet()) {
+                int count = nextWords.get(nextWord);
+                System.out.print(nextWord + "(" + count + ") ");
+            }
+            System.out.println();
+        }
+    }
 }
+
+			
+		
+		
+	  
+	  
+
---
+++ java/src/ie/tudublin/Word.java
@@ -0,0 +1,46 @@
+package ie.tudublin;
+import java.util.ArrayList;
+
+
+public class Word {
+    private String word;
+    private ArrayList<Follow> follows;
+
+    public Word(String word) {
+        this.word = word;
+        this.follows = new ArrayList<>();
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public ArrayList<Follow> getFollows() {
+        return follows;
+    }
+
+    public void addFollow(Follow follow) {         //add a Follow object to the follows list.
+        follows.add(follow);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(word).append(": ");
+        for (Follow follow : follows) {
+            sb.append(follow).append(", ");       //separated by commas and spaces. 
+        }
+        sb.delete(sb.length() - 2, sb.length()); // remove the last comma and space
+        return sb.toString();
+    }
+    public Follow findFollow(String str) {
+        for (Follow w : follows) {
+            if (w.getWord().equals(str)) {
+                return w;
+            }
+        }
+        return null;
+    }
+    
+}
+
